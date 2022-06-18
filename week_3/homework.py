3ca9f0103bf6371a7b070b30293ccae744375d22
Moiz Sajid <moizsajid619@gmail.com>
2022-06-18 17:32:35 +0500

    feat(week_3): Updated week_3/homework.py

--- week_3/homework.py
+++ week_3/homework.py
@@ -4,11 +4,11 @@ from sklearn.feature_extraction import DictVectorizer
 from sklearn.linear_model import LinearRegression
 from sklearn.metrics import mean_squared_error
 
-from prefect import flow, task, context
+from prefect import flow, task, get_run_logger
 from prefect.task_runners import SequentialTaskRunner
 from datetime import datetime
 from dateutil.relativedelta import relativedelta
-import urllib
+import pickle
 
 @task(retries=3, retry_delay_seconds=60)
 def read_data(path):
@@ -17,22 +17,23 @@ def read_data(path):
 
 @task
 def prepare_features(df, categorical, train=True):
+    logger = get_run_logger()
     df['duration'] = df.dropOff_datetime - df.pickup_datetime
     df['duration'] = df.duration.dt.total_seconds() / 60
     df = df[(df.duration >= 1) & (df.duration <= 60)].copy()
 
     mean_duration = df.duration.mean()
     if train:
-        print(f"The mean duration of training is {mean_duration}")
+        logger.info(f"The mean duration of training is {mean_duration}")
     else:
-        print(f"The mean duration of validation is {mean_duration}")
+       logger.info(f"The mean duration of validation is {mean_duration}")
     
     df[categorical] = df[categorical].fillna(-1).astype('int').astype('str')
     return df
 
 @task
 def train_model(df, categorical):
-    logger = context.get("logger")
+    logger = get_run_logger()
     train_dicts = df[categorical].to_dict(orient='records')
     dv = DictVectorizer()
     X_train = dv.fit_transform(train_dicts) 
@@ -48,8 +49,9 @@ def train_model(df, categorical):
     logger.info(f"The MSE of training is: {mse}")
     return lr, dv
 
+@task
 def run_model(df, categorical, dv, lr):
-    logger = context.get("logger")
+    logger = get_run_logger()
     val_dicts = df[categorical].to_dict(orient='records')
     X_val = dv.transform(val_dicts) 
     y_pred = lr.predict(X_val)
@@ -59,7 +61,7 @@ def run_model(df, categorical, dv, lr):
     logger.info(f"The MSE of validation is: {mse}")
     return
 
-@flow(task_runner=SequentialTaskRunner())
+@flow
 def main(date=None):
 
     datetime_object = datetime.strptime(date, "%Y-%m-%d")
@@ -70,19 +72,37 @@ def main(date=None):
     train_month, train_year = str(train_date.month).zfill(2), str(train_date.year)
     val_month, val_year = str(val_date.month).zfill(2), str(val_date.year)
         
-    train_path: str = f"https://nyc-tlc.s3.amazonaws.com/trip+data/fhv_tripdata_{train_year}-{train_month}.parquet"
-    val_path: str = f"https://nyc-tlc.s3.amazonaws.com/trip+data/fhv_tripdata_{val_year}-{val_month}.parquet"
+    train_path: str = f"http://nyc-tlc.s3.amazonaws.com/trip+data/fhv_tripdata_{train_year}-{train_month}.parquet"
+    val_path: str = f"http://nyc-tlc.s3.amazonaws.com/trip+data/fhv_tripdata_{val_year}-{val_month}.parquet"
 
     categorical = ['PUlocationID', 'DOlocationID']
 
     df_train = read_data(train_path)
-    df_train_processed = prepare_features(df_train, categorical).result()
+    df_train_processed = prepare_features(df_train, categorical)
 
     df_val = read_data(val_path)
-    df_val_processed = prepare_features(df_val, categorical, False).result()
+    df_val_processed = prepare_features(df_val, categorical, False)
 
     # train the model
-    lr, dv = train_model(df_train_processed, categorical)
+    lr, dv = train_model(df_train_processed, categorical).result()
     run_model(df_val_processed, categorical, dv, lr)
 
-main(date="2021-08-15")
+    with open(f"models/model-{date}.bin", "wb") as f_out:
+        pickle.dump(lr, f_out)
+
+    with open(f"models/dv-{date}.b", "wb") as f_out:
+        pickle.dump(dv, f_out)
+
+
+from prefect.deployments import DeploymentSpec
+from prefect.orion.schemas.schedules import CronSchedule
+from prefect.flow_runners import SubprocessFlowRunner
+from datetime import timedelta
+
+DeploymentSpec(
+    flow=main,
+    name="model_training",
+    schedule=CronSchedule(cron="0 9 15 * *", timezone="America/New_York"),
+    flow_runner=SubprocessFlowRunner(),
+    tags=["ml"]
+)
